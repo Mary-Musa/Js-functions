e44f951b3b4399fcda515d4b25246b780b6b8987
Mary-Musa <kakamary1@gmail.com>
2026-05-05 14:12:27 +0300

    modified files : constructors.js,day2classassignments.js and index.html. Also a new file day2assignment was created

--- constructors.js
+++ constructors.js
@@ -8,19 +8,42 @@ function Person(name,age,role){ //Person is the blueprint-thatbhelps us to creat
     this.name = name;//this means the new object is being created.
     this.age = age;
     this.role = role;
+
+
+
+    
+    //this is a method -which is a function inside an object
+
+
+                    this.introduce = function(){
+
+                    return `Hi,I am ${name} and am ${age} years old . My role is ${role}. `;
+                }
+
+//This is a method to check whether a person is an adult or a minor and prints out an into message .
+
+            this.isAdult = function(){
+
+                if(age>=18){
+                    return `Hi! ${name} ,you are an adult`;
+                }else{
+
+                    return `Hi!, ${name} ,you are a minor`;
+                }
+            }
 }
 
 const user1 = new Person("Mary",27,"software developer");
-const user2 = new Person("Nancy",30,"Manager");
+const user2 = new Person("Nancy",12,"Manager");
 const user3 = new Person("John",35,"Seniour Manager");
 
 
-console.log(user1);
-console.log(user2);
-console.log(user3);
+console.log(user1.isAdult());
+console.log(user2.isAdult());
+console.log(user3.isAdult());
 
 
-// class assignment
+// class assignment/tasks
 
 
 function Product(productName,price,quantity){
@@ -34,6 +57,7 @@ function Product(productName,price,quantity){
 
     //     return this.price * this.quantity;
     // }
+
 }
 
 
