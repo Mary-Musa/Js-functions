eab07c5d327d10a031893f14a4d443dd231be9df
Mary-Musa <kakamary1@gmail.com>
2026-05-05 16:03:10 +0300

    added task2 for day2assignments

--- day2assignments.js
+++ day2assignments.js
@@ -37,5 +37,38 @@ const b = ["Lemon","Pears","Banana"];
 console.log(combineArrays(a,b));
 
 
+//Task 2
+
+
+/*
+Task 2:
+
+Write a function called createGreeting that takes two parameters:
+
+name (default value: "Guest")
+
+greeting (default value: "Hello")
+
+Requirements:
+
+If no arguments are passed, return:
+"Hello, Guest!"
+
+If only name is passed, use the default greeting.
+
+If both arguments are passed, use both custom values.
+
+*/
+
+function createGreeting(name="Guest",greeting="Hello"){
+
+
+    return `${greeting} ${name}`;
+}
+
+
+console.log(createGreeting("Nelson"));
+console.log(createGreeting("Maria","Hi"));
+console.log(createGreeting());
 
 
